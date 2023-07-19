676072e3f9abbb3c5bdd2b8c811d709e5e1756f2
Scott Byrne <sbyrne@buildingwings.com>
2023-07-19 14:22:06 -0700

    Created 1 page for index and 1 for schedule creator

---
+++ scheduleCreator.js
@@ -0,0 +1,7 @@
+"use strict";
+window.onload = function () {
+  const fileInput = document.getElementById("series-file");
+  const totalGamesDialog = document.getElementById("games-label");
+  const daysInput = document.getElementById("days");
+  const createScheduleButton = document.getElementById("create-schedule");
+};
